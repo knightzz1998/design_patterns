a487e9098d6da463e83575f42da4919fbfab90db
王天赐 <15565946702@163.com>
2022-01-22 12:02:51 +0800

    :art: init design pattern

---
+++ src/main/java/cn/knightzz/pattern/adapter/class_adapter/Client.java
@@ -0,0 +1,13 @@
+package cn.knightzz.pattern.adapter.class_adapter;
+
+/**
+ * @author 王天赐
+ * @title: Client
+ * @projectName design_patterns
+ * @description:
+ * @website http://knightzz.cn/
+ * @github https://github.com/knightzz1998
+ * @date 2022/1/22 12:01
+ */
+public class Client {
+}
---
+++ src/main/java/cn/knightzz/pattern/adapter/class_adapter/Computer.java
@@ -0,0 +1,13 @@
+package cn.knightzz.pattern.adapter.class_adapter;
+
+/**
+ * @author 王天赐
+ * @title: Computer
+ * @projectName design_patterns
+ * @description:
+ * @website http://knightzz.cn/
+ * @github https://github.com/knightzz1998
+ * @date 2022/1/22 12:02
+ */
+public class Computer {
+}
